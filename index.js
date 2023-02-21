d9914d2f80bd9762b498228ea056901647116139
Sarthakw001 <sarthakw01@gmail.com>
2023-02-22 02:54:43 +0530

    Minor Changes

--- index.js
+++ index.js
@@ -17,12 +17,11 @@ import { verifyToken } from "./middleware/auth.js";
 // import User from "./models/User.js";
 // import Post from "./models/Post.js";
 // import { users, posts } from "./data/index.js";
-
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config();
 const app = express();
+dotenv.config();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -52,8 +51,7 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
